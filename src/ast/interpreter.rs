f3a9545afbd73890c8a5e13a800cd688099e26a0
zzzdong <kuwater@163.com>
2025-01-23 16:13:02 +0800

    feat: ast's Interpreter add support call fn

--- src/ast/interpreter.rs
+++ src/ast/interpreter.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 
-
 use crate::{
     vm::{Enumerator, Environment, Primitive, Range},
     Error, Null, Object, RuntimeError, Value, ValueRef,
@@ -37,10 +36,20 @@ impl Variables {
     }
 
     pub fn set(&mut self, name: impl AsRef<str>, value: ValueRef) {
-        self.scopes
-            .last_mut()
-            .unwrap()
-            .insert(name.as_ref().to_string(), value);
+        for scope in self.scopes.iter_mut().rev() {
+            if let Some(v) = scope.get_mut(name.as_ref()) {
+                *v = value;
+                return;
+            }
+        }
+    }
+
+    pub fn enter_scope(&mut self) {
+        self.scopes.push(HashMap::new());
+    }
+
+    pub fn exit_scope(&mut self) {
+        self.scopes.pop();
     }
 }
 
@@ -55,6 +64,7 @@ enum ControlFlow {
 pub struct Interpreter {
     variables: Variables,
     return_value: Option<ValueRef>,
+    functions: HashMap<String, FunctionItem>,
 }
 
 impl Interpreter {
@@ -62,6 +72,7 @@ impl Interpreter {
         Self {
             variables: Variables::new(),
             return_value: None,
+            functions: HashMap::new(),
         }
     }
 
@@ -73,13 +84,12 @@ impl Interpreter {
 
     pub fn eval_program(&mut self, program: Program) -> Result<(), Error> {
         let mut stmts = Vec::new();
-        let mut function_map = HashMap::new();
 
         // split program into statements and items
         for stmt in program.stmts {
             match stmt {
                 Statement::Item(ItemStatement::Fn(func)) => {
-                    function_map.insert(func.name.clone(), func);
+                    self.functions.insert(func.name.clone(), func);
                 }
                 Statement::Item(_) => {
                     unimplemented!("unsupported item statement")
@@ -91,20 +101,32 @@ impl Interpreter {
             }
         }
 
-        self.eval_statements(&stmts)?;
+        for stmt in stmts {
+            match self.eval_statement(&stmt)? {
+                ControlFlow::Next => {}
+                _ => {
+                    break;
+                }
+            }
+        }
 
         Ok(())
     }
 
     fn eval_statements(&mut self, stmts: &[Statement]) -> Result<ControlFlow, Error> {
-        self.variables.scopes.push(HashMap::new());
+        self.variables.enter_scope();
 
         for stmt in stmts {
             match self.eval_statement(stmt)? {
                 ControlFlow::Next => {}
-                control => return Ok(control),
+                control => {
+                    self.variables.exit_scope();
+                    return Ok(control);
+                }
             }
         }
+
+        self.variables.exit_scope();
 
         Ok(ControlFlow::Next)
     }
@@ -250,7 +272,64 @@ impl Interpreter {
                     _ => unimplemented!("unsupported assignment expression: {:?}", object),
                 }
             }
+            Expression::Call(CallExpression { func, args }) => {
+                self.eval_call_expression(func, args)
+            }
             _ => unimplemented!("unsupported expression: {:?}", expr),
+        }
+    }
+
+    fn eval_call_expression(
+        &mut self,
+        func: &Expression,
+        args: &[Expression],
+    ) -> Result<ValueRef, Error> {
+        match func {
+            Expression::Identifier(ident) => {
+                if let Some(mut func) = self.variables.get(ident.0.as_str()) {
+                    let args = args
+                        .iter()
+                        .map(|arg| self.eval_expression(arg))
+                        .collect::<Result<Vec<_>, _>>()?;
+                    let ret = func
+                        .get_mut()
+                        .call(&args)
+                        .map(|ret| ret.unwrap_or(Null.into()))?;
+                    Ok(ValueRef::new(ret))
+                } else {
+                    match self.functions.get(ident.0.as_str()).cloned() {
+                        Some(FunctionItem {
+                            name,
+                            params,
+                            return_ty,
+                            body,
+                        }) => {
+                            self.variables.enter_scope();
+                            let args = args
+                                .iter()
+                                .map(|arg| self.eval_expression(arg))
+                                .collect::<Result<Vec<_>, _>>()?;
+                            for (param, arg) in params.iter().zip(args.iter()) {
+                                self.variables.define(param.name.as_str(), arg.clone());
+                            }
+
+                            match self.eval_statements(&body)? {
+                                ControlFlow::Return(ret) => {
+                                    self.variables.exit_scope();
+                                    return Ok(ret.unwrap_or(ValueRef::new(Null.into())));
+                                }
+                                _ => {}
+                            }
+                            self.variables.exit_scope();
+                            return Ok(ValueRef::new(Null.into()));
+                        }
+                        None => {
+                            return Err(RuntimeError::symbol_not_found(ident.0.as_str()).into());
+                        }
+                    }
+                }
+            }
+            _ => unimplemented!("unsupported call expression: {:?}", func),
         }
     }
 
@@ -506,84 +585,6 @@ mod tests {
         );
         let value = interp.eval_expression(&expr).unwrap();
         assert_eq!(value, true);
-
-        // 测试赋值运算符
-        let expr = Expression::Binary(
-            BinOp::Assign,
-            Box::new(Expression::Identifier(IdentifierExpression(
-                "a".to_string(),
-            ))),
-            Box::new(Expression::Literal(LiteralExpression::Integer(5))),
-        );
-        let value = interp.eval_expression(&expr).unwrap();
-        assert_eq!(value, Null);
-        let var_value = interp.variables.get("a").unwrap();
-        assert_eq!(var_value, 5);
-
-        // 测试加法赋值运算符
-        let expr = Expression::Binary(
-            BinOp::AddAssign,
-            Box::new(Expression::Identifier(IdentifierExpression(
-                "a".to_string(),
-            ))),
-            Box::new(Expression::Literal(LiteralExpression::Integer(3))),
-        );
-        let value = interp.eval_expression(&expr).unwrap();
-        assert_eq!(value, Null);
-        let var_value = interp.variables.get("a").unwrap();
-        assert_eq!(var_value, 8);
-
-        // 测试减法赋值运算符
-        let expr = Expression::Binary(
-            BinOp::SubAssign,
-            Box::new(Expression::Identifier(IdentifierExpression(
-                "a".to_string(),
-            ))),
-            Box::new(Expression::Literal(LiteralExpression::Integer(2))),
-        );
-        let value = interp.eval_expression(&expr).unwrap();
-        assert_eq!(value, Null);
-        let var_value = interp.variables.get("a").unwrap();
-        assert_eq!(var_value, 6);
-
-        // 测试乘法赋值运算符
-        let expr = Expression::Binary(
-            BinOp::MulAssign,
-            Box::new(Expression::Identifier(IdentifierExpression(
-                "a".to_string(),
-            ))),
-            Box::new(Expression::Literal(LiteralExpression::Integer(2))),
-        );
-        let value = interp.eval_expression(&expr).unwrap();
-        assert_eq!(value, Null);
-        let var_value = interp.variables.get("a").unwrap();
-        assert_eq!(var_value, 12);
-
-        // 测试除法赋值运算符
-        let expr = Expression::Binary(
-            BinOp::DivAssign,
-            Box::new(Expression::Identifier(IdentifierExpression(
-                "a".to_string(),
-            ))),
-            Box::new(Expression::Literal(LiteralExpression::Integer(3))),
-        );
-        let value = interp.eval_expression(&expr).unwrap();
-        assert_eq!(value, Null);
-        let var_value = interp.variables.get("a").unwrap();
-        assert_eq!(var_value, 4);
-
-        // 测试取模赋值运算符
-        let expr = Expression::Binary(
-            BinOp::ModAssign,
-            Box::new(Expression::Identifier(IdentifierExpression(
-                "a".to_string(),
-            ))),
-            Box::new(Expression::Literal(LiteralExpression::Integer(3))),
-        );
-        let value = interp.eval_expression(&expr).unwrap();
-        assert_eq!(value, Null);
-        let var_value = interp.variables.get("a").unwrap();
-        assert_eq!(var_value, 1);
     }
 
     // 添加对 if 语句的测试用例
@@ -669,5 +670,29 @@ mod tests {
         interp.eval(script).unwrap();
         let var_value = interp.variables.get("a").unwrap();
         assert_eq!(var_value, 2);
+    }
+
+    #[test]
+    fn test_eval_fn() {
+        let mut interp = Interpreter::new();
+
+        // 测试 for 循环中带有 continue 语句
+        let script = r#"
+        fn fib(n) {
+            if n < 1 {
+                return 0;
+            }
+            if n <= 2 {
+                return 1;
+            }
+
+            return fib(n - 1) + fib(n - 2);
+        }
+
+        let a = fib(2);
+        "#;
+        interp.eval(script).unwrap();
+        let var_value = interp.variables.get("a").unwrap();
+        assert_eq!(var_value, 1);
     }
 }
